645403f2b400fd0671ed06cebafab45a6a01490c
Yelbek05 <yelbekmaldabayev@gmail.com>
2025-01-10 20:34:02 +0500

    Create dev-core-101-task-python.py

---
+++ Devcore101/dev-core-101-task-python.py
@@ -0,0 +1,11 @@
+# Приготовить яичницу.
+print("put the frying pan on the stove")
+print("turn on the stove's gas")
+print("put the butter or meal oil on the pan")
+print("break the eggs")
+print("put eggs on the frying pan")
+print("sprinkle salt on eggs")
+print("wait up to 20 seconds")
+print("turn the eggs upside down")
+print("wait up to 20 seconds")
+#Enjoy your meal!
