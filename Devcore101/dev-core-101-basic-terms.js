ee9b73d7fe4cc40e5e52a2bb7113ab882e6cfd4b
Yelbek05 <yelbekmaldabayev@gmail.com>
2025-01-10 20:35:11 +0500

    Update dev-core-101-basic-terms.js

--- Devcore101/dev-core-101-basic-terms.js
+++ Devcore101/dev-core-101-basic-terms.js
@@ -1,5 +1,6 @@
-1) Let angle_velocity = 20;
-2) 
+// 1) 
+Let angle_velocity = 20;
+// 2) 
 function do_this(){
   (for(let i=4; i <= 4; i++)){
     console.log(i);
@@ -7,7 +8,7 @@ function do_this(){
 }
 do_this();
 
-3) 
+// 3) 
 if (a+b>10){
   console.log("need less than 10");
 }
@@ -16,14 +17,19 @@ else(){
   console.log("need more than 10")
 }
 
-4)
+// 4)
 for(let i = 10; i < 11; i++){
   console.log(i);
 }
 
-5) let a = 12;
-6) let res = 4 + 3;
-7) let consumer_age = 20;
-8) let last_name = "Maldabayev";
-9) // this is a comment, isn't?
-10) let isMale = True;
+// 5) 
+let a = 12;
+// 6) 
+let res = 4 + 3;
+// 7)
+let consumer_age = 20;
+// 8)
+let last_name = "Maldabayev";
+// 9) // this is a comment, isn't?
+// 10)
+let isMale = True;
